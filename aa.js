24f10ab2ec7a19845c4c2e203e00ba6536f82539
bold24-AY <itsayush24may@gmail.com>
2024-10-30 21:21:25 +0530

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,5 @@
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We’ll get back to you soon.");
+  });
